df26c76df7bf39dd2f3067833050d6078b86491b
Bishal Gautam <bisalgt@gmail.com>
2020-07-24 19:28:41 +0545

    learn filters and context processor

--- practice/l_template.py
+++ practice/l_template.py
@@ -11,3 +11,18 @@ def index():
 def home():
     value = Markup('<h2>THis is markup object for autoescaping off, enable html externally. </h2>')
     return render_template('l_template.html', value=value)
+
+@app.template_filter('capitalize')
+def capitalize_data(value):
+    return value.upper()
+
+
+def lower_value(val):
+    return f'{val.lower()} hello there lower value'
+app.jinja_env.filters['lower'] = lower_value 
+
+
+
+@app.context_processor
+def myname():
+    return {'name':'Bishal Gautam', 'age':24}
